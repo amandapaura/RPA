0d5156c22a35cdb5e5245eddb190a6df0747f016
Amanda Paura <166857850+amandapaura@users.noreply.github.com>
2025-05-19 12:39:03 -0400

    Update api_powerbi.py
    update

--- api_powerbi.py
+++ api_powerbi.py
@@ -1,147 +1,115 @@
-#-- ---------------------------------------------------------------------------------------
-import requests
-import json
-import pbirest
-import msal
-from extensao import *;
-from tqdm import tqdm
 import os
+import time
 import datetime
-import keyring as kr
+import requests
+import msal
 
+class PowerBIExporter:
+    def __init__(self, kr, base_path='C:/user/amandapaura/backup/PowerBi'):
+        self.kr = kr
+        self.base_path = base_path
+        self.tenant_id = kr.get_password('PowerBI', 'tenant_id')
+        self.client_id = kr.get_password('PowerBI', 'client_id')
+        self.client_secret = kr.get_password('PowerBI', 'client_secret')
+        self.authority = f'https://login.microsoftonline.com/{self.tenant_id}'
+        self.scope = ['https://analysis.windows.net/powerbi/api/.default']
+        self.token = self.get_token()
 
-tenant_id     = kr.get_password('PowerBI', 'tenant_id')
-client_id     = kr.get_password('PowerBI', 'client_id')
-client_secret = kr.get_password('PowerBI', 'client_secret')
+    def get_token(self):
+        app = msal.ConfidentialClientApplication(
+            client_id=self.client_id,
+            authority=self.authority,
+            client_credential=self.client_secret
+        )
 
-base_path = 'C:/user/amandapaura/backup/PowerBi'
-authority = f'https://login.microsoftonline.com/{tenant_id}'
-scope = ['https://analysis.windows.net/powerbi/api/.default']
+        user = self.kr.get_password('PowerBI', 'user')
+        password = self.kr.get_password('PowerBI', 'password')
+        result = app.acquire_token_by_username_password(user, password, scopes=self.scope)
 
-# ---------------- Funções ----------------------------
+        if "access_token" in result:
+            return result['access_token']
+        else:
+            raise Exception("Erro ao adquirir token de acesso.")
 
-# getting token
-def get_token(): 
-    app = msal.ConfidentialClientApplication(client_id=client_id, 
-                                             authority=authority, 
-                                             client_credential=client_secret)
-    #result = app.acquire_token_for_client(scopes=scope)
+    def get_workspaces(self):
+        url = 'https://api.powerbi.com/v1.0/myorg/groups'
+        headers = {'Authorization': f'Bearer {self.token}'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
 
-    user     = kr.get_password('PowerBI', 'user')
-    password = kr.get_password('PowerBI', 'password')
-    result   = app.acquire_token_by_username_password(user,password,scopes=scope)
+    def get_reports(self, group_id):
+        url = f'https://api.powerbi.com/v1.0/myorg/groups/{group_id}/reports'
+        headers = {'Authorization': f'Bearer {self.token}'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
 
-    if "access_token" in result:
-        return result['access_token']
+    def get_backup_path(self, workspace_name):
+        today = datetime.date.today()
 
+        if today.day == 1:
+            month_folder = today.strftime("%Y-%m")
+            path = os.path.join(self.base_path, 'month', month_folder, workspace_name)
+        else:
+            week_number = today.strftime("%Y-%W")
+            day_name = today.strftime("%A").lower()
+            path = os.path.join(self.base_path, 'week', week_number, day_name, workspace_name)
 
-# listing workspaces
-def get_workspaces(token):    
-    url = 'https://api.powerbi.com/v1.0/myorg/groups'
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    #print(response.json())
-    print('\n\n')
-    return response.json()
+        os.makedirs(path, exist_ok=True)
+        return path
 
-# listing reports
-def get_reports(token, group_id):    
-    url = f'https://api.powerbi.com/v1.0/myorg/groups/{group_id}/reports'
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    #print(response.json())
-    print('\n\n')
+    def is_exportable_report(self, group_id, report_id):
+        headers = {'Authorization': f'Bearer {self.token}'}
 
-    return response.json()
+        report_url = f'https://api.powerbi.com/v1.0/myorg/groups/{group_id}/reports/{report_id}'
+        report_response = requests.get(report_url, headers=headers)
+        report = report_response.json()
 
-## creating path for saving monthly backups (every 1st day) and daily (organized by week)
-def get_backup_path(workspace_name):
-    today = datetime.date.today()
+        dataset_id = report.get('datasetId')
+        if not dataset_id:
+            return False, "Reports without dataset - PBIX cannot be exported"
 
-    if today.day == 1:
-        # Backup mensal
-        month_folder = today.strftime("%Y-%m")
-        path = os.path.join(base_path, 'month', month_folder, workspace_name)
-    else:
-        # Backup semanal
-        week_number = today.strftime("%Y-%W")
-        day_name = today.strftime("%A").lower()
-        path = os.path.join(base_path, 'week', week_number, day_name, workspace_name)
+        dataset_url = f'https://api.powerbi.com/v1.0/myorg/groups/{group_id}/datasets/{dataset_id}'
+        dataset_response = requests.get(dataset_url, headers=headers)
+        dataset = dataset_response.json()
 
-    os.makedirs(path, exist_ok=True)
-    return path
+        if dataset.get('datasetMode') != 'Import':
+            return False, f"Dataset in {dataset.get('datasetMode')} mode - PBIX can't be exported"
 
+        return True, "OK"
 
-def is_exportable_report(token, group_id, report_id):
-    headers = {'Authorization': f'Bearer {token}'}
-    report_url = f'https://api.powerbi.com/v1.0/myorg/groups/{group_id}/reports/{report_id}'
-    report_response = requests.get(report_url, headers=headers)
-    report = report_response.json()
+    def export_report(self, group_id, report_id, report_name, output_dir):
+        exportable, reason = self.is_exportable_report(group_id, report_id)
 
-    #print(report)
+        if not exportable:
+            msg = f"Relatório {report_name} não pode ser exportado: {reason}"
+            return {'status': 'skipped', 'message': msg}
 
-    # if report.get('isFromPbix', False):
-    #     return False, "Relatório não foi gerado por PBIX - não pode exportar PBIX"
+        url = f'https://api.powerbi.com/v1.0/myorg/reports/{report_id}/Export'
+        headers = {'Authorization': f'Bearer {self.token}', 'Content-Type': 'application/json'}
+        body = {"format": "PBIX"}
 
-    # if report.get('isPaginated', False):
-    #     return False, "Relatório paginado - não pode exportar PBIX"
-    
+        response = requests.post(url, headers=headers, json=body)
 
-    dataset_id = report.get('datasetId')
-    dataset_url = f'https://api.powerbi.com/v1.0/myorg/datasets/{dataset_id}'
-    dataset_response = requests.get(dataset_url, headers=headers)
-    dataset = dataset_response.json()
-    #print(dataset)
+        if response.status_code == 202:
+            export_url = response.headers.get('Location')
+            print(f'Exportando {report_name}')
+            
+            while True:
+                r = requests.get(export_url, headers=headers, stream=True)
+                if r.status_code == 200:
+                    file_path = os.path.join(output_dir, f'{report_name}.pbix')
+                    with open(file_path, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=1024 * 1024):
+                            f.write(chunk)
+                    print(f'{report_name}.pbix salvo com sucesso')
+                    break
+                elif r.status_code == 202:
+                    time.sleep(2)
+                else:
+                    print(f"Erro ao exportar {report_name}: {r.status_code} - {r.text}")
+                    break
+        else:
+            print(f"Erro ao iniciar exportação: {response.status_code} - {response.text}")
 
-    if not dataset_id:
-        return False, "Reports withou dataset - PBIX can not be exported"
-
-    dataset_url = f'https://api.powerbi.com/v1.0/myorg/groups/{group_id}/datasets/{dataset_id}'
-    dataset_response = requests.get(dataset_url, headers=headers)
-    dataset = dataset_response.json()
-
-    print(dataset.get('mode'))
-
-    if dataset.get('datasetMode') != 'Import':
-        return False, f"Dataset as {dataset.get('datasetMode')} mode - PBIX can't be exported"
-
-    return True, "OK"
-
-## function to export pbix
-def export_report(token, group_id, report_id, report_name, output_dir):
-    import time
-    exportable, reason = is_exportable_report(token, group_id, report_id)
-
-    if not exportable:
-        msg= f"Relatório {report_name} não pode ser exportado: {reason}"
-        #print(msg)
-        return {'status': 'skipped', 'message': msg}
-
-    url = f'https://api.powerbi.com/v1.0/myorg/reports/{report_id}/Export' #f'https://api.powerbi.com/v1.0/myorg/groups/{group_id}/reports/{report_id}/ExportTo'
-    headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
-    body = {"format": "PBIX"}
-
-    response = requests.post(url, headers=headers, json=body)
-
-    if response.status_code == 202:
-        export_url = response.headers.get('Location')
-        print(f'Exportando {report_name}')
-        
-        while True:
-            r = requests.get(export_url, headers=headers, stream=True)
-            if r.status_code == 200:
-                file_path = os.path.join(output_dir, f'{report_name}.pbix')
-                with open(file_path, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=1024*1024):
-                        f.write(chunk)
-                print(f'{report_name}.pbix salvo com sucesso')
-                break
-            elif r.status_code == 202:
-                time.sleep(2)
-            else:
-                print(f"Error to export {report_name}: {r.status_code} - {r.text}")
-                break
-    else:
-        print(f"Error starting export: {response.status_code} - {response.text}")
