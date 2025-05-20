086c7951f6606da303a2d5822ad700027f8f457e
Amanda Paura <166857850+amandapaura@users.noreply.github.com>
2025-05-20 08:14:56 -0400

    Update main.py
    final edit

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import os
 from pbi_web_scraping import PowerBIScraper
 from pbi_workspaces_reports import PowerBIReportCollector
 
-
 if __name__ == '__main__':
     base_path = 'C:/user/amandapaura/backup/PowerBi' #example 
-    json_path = os.path.join(base_path, 'workspaces_relatorios.json')
+    json_path = os.path.join(base_path, 'workspaces_reports.json')
     chrome_driver_path = "C:/chromedriver/chromedriver.exe"
     user_email = "email@email.com"
-
-    # Passo 1: Gerar JSON com os relatórios a partir da API
+    
+    print("Iniciando a coleta de URLS")
+    # Gerar JSON com os relatórios a partir da API
     collector = PowerBIReportCollector(user_email)
     collector.run()
-    
-    # Passo 2: Executar o scraping no Power BI Web
+
+    print('Iniciando Scrapping')
+    # Executar o scraping no Power BI Web
     scraper = PowerBIScraper(
         user=user_email,
         json_path=json_path,
         chrome_driver_path=chrome_driver_path
     )
     scraper.run()
+
